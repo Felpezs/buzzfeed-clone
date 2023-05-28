ed44a8c1d6d0ed3a52d6cc1e4449c087994f79f6
Felipe <felipe.edfreire@gmail.com>
2023-05-28 00:41:16 -0300

    mudanca na estrutura do json e formato do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -10,8 +10,8 @@ export class QuizzComponent implements OnInit {
   title: string = '';
   questions: any;
   questionSelected: any;
-  answers: Array<string> = [];
-  answerSelected: string = '';
+  answers: number = 0;
+  answerSelected: { mob: string; description: string} | any;
   questionIndex: number = 0;
   questionMaxIndex: number = 0;
   finished: boolean = false;
@@ -29,7 +29,7 @@ export class QuizzComponent implements OnInit {
   }
 
   playerChoose(value: string) {
-    this.answers.push(value);
+    this.answers += Number(value);
     this.nextStep();
   }
 
@@ -39,27 +39,17 @@ export class QuizzComponent implements OnInit {
     if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex];
     } else {
-      const finalAnswer: string = await this.checkResult(this.answers);
-      this.answerSelected =
-        quizz_questions.results[
-          finalAnswer as keyof typeof quizz_questions.results
-        ];
+      this.answerSelected = await this.checkResult(this.answers);
       this.finished = true;
     }
   }
 
-  async checkResult(answers: Array<string>) {
-    const result = answers.reduce((previous, current, i, arr) => {
-      if (
-        arr.filter((item) => item === previous).length >
-        arr.filter((item) => item === current).length
-      ) {
-        return previous;
-      } else {
-        return current;
-      }
-    });
-
-    return result;
+  async checkResult(result: number) {
+    if (result < 3) return quizz_questions.results[0];
+    if (result >= 3 && result < 5) return quizz_questions.results[1];
+    if (result >= 5 && result < 8) return quizz_questions.results[2];
+    if (result >= 8 && result < 10) return quizz_questions.results[3];
+    if (result >= 10 && result < 12) return quizz_questions.results[4];
+    else return quizz_questions.results[5];
   }
 }
